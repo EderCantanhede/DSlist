befd90b2dedbc83c7683a8cb2b98578299296640
eder <edercantanhede@gmail.com>
2024-11-16 23:36:13 -0300

    Games Projections

---
+++ src/main/java/com/devsuperior/dslist/prijections/GameMinProjection.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dslist.prijections;
+
+public interface GameMinProjection {
+	Long getId();
+	String getTitle();
+	Integer getYear();
+	String getImgUrl();
+	String getSortDescription();
+	Integer getPosition();
+}
